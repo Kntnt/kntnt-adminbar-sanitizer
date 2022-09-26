928566af5b20a934c879cd1da9b543f99e19d8fd
Thomas Barregren <thomas@kntnt.com>
2022-09-26 14:42:18 +0200

    Refactored from regular plugin to mu-plugin.

--- README.md
+++ README.md
@@ -1,27 +1,23 @@
-# Kntnt's adminbar sanitizer
+# Kntnt Adminbar Sanitizer
 
-WordPress plugin that sanitizes admin bar from junk and provides enhancements.
+WordPress mu-plugin that sanitizes admin bar from junk and provides enhancements.
 
 ## Description
 
-You can use this plugin to reomve or add any ites to the admin bar.
+You can use this plugin to add or reomve items to the admin bar.
 
 Out of the box, this plugns adds a toggle button, so you can switch between frontend and admin, and removes following buttons
 
 * WordPress (menu with About, WordPress.org, Documentation, Support and Feedback)
 * Site menu (menu with Visit site alternative Panel, Theme, Widgets and Menus)
-* Customize
 
 You can add and delete items from the list of items to be removed by implementing the `kntnt_admin_bar_sanitizer_remove_list`-filter:
 
 ```php
 add_filter('kntnt_admin_bar_sanitizer_remove_list', function($remove_list) {
-  /* Here you can add and delete items from the $remove_list array
-   *
-   *   $remove_list[] = 'my-account';
-   *
-   */
-  return $remove_list;
+    $remove_list[] = 'customize';
+    $remove_list[] = 'my-account';
+    return $remove_list;
 });
 ```
 
@@ -29,17 +25,13 @@ You can add and delete items from the list of items to be added on the frontend 
 
 ```php
 add_filter('kntnt_admin_bar_sanitizer_add_front_list', function($add_list) {
-  /* Here you can add and delete items from the $add_list array.
-   *
-   *   $add_list['kntnt'] = [
-   *     'title' => 'Kntnt',
-   *     'link' => 'https://www.kntnt.com/',
-   *     'icon' => '\f102',       // OPTIONAL
-   *     'icon_top_margin' => 0,  // OPTIONAL
-   *   ],
-   *
-  */
-  return $add_list;
+    $add_list['kntnt'] = [
+        'title' => 'Kntnt',
+        'link' => 'https://www.kntnt.com/',
+        'icon' => '\f102',       // OPTIONAL
+        'icon_top_margin' => 0,  // OPTIONAL
+     ],
+    return $add_list;
 });
 ```
 
@@ -47,16 +39,12 @@ You can add and delete items from the list of items to be added on the backend b
 
 ```php
 add_filter('kntnt_admin_bar_sanitizer_add_admin_list', function($add_list) {
-  /* Here you can add and delete items from the $add_list array.
-   *
-   *   $add_list['kntnt'] = [
-   *     'title' => 'Kntnt',
-   *     'link' => 'https://www.kntnt.com/',
-   *     'icon' => '\f102',       // OPTIONAL
-   *     'icon_top_margin' => 0,  // OPTIONAL
-   *   ],
-   *
-  */
+    $add_list['kntnt'] = [
+        'title' => 'Kntnt',
+        'link' => 'https://www.kntnt.com/',
+        'icon' => '\f102',       // OPTIONAL
+        'icon_top_margin' => 0,  // OPTIONAL
+    ],
   return $add_list;
 });
 ```
--- kntnt-adminbar-sanitizer.php
+++ kntnt-adminbar-sanitizer.php
@@ -1,87 +1,198 @@
 <?php
 
 /**
- * Plugin main file.
- *
  * @wordpress-plugin
- * Plugin Name:       Kntnt's adminbar sanitizer
+ * Plugin Name:       Kntnt Adminbar Sanitizer
  * Plugin URI:        https://www.kntnt.com/
  * Description:       Sanitizes adminbar from junk and provides enhancements.
- * Version:           1.0.1
+ * Version:           2.0.0
  * Author:            Thomas Barregren
  * Author URI:        https://www.kntnt.com/
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  */
- 
-namespace Kntnt\Admin_Bar_Sanitizer;
 
-defined('WPINC') || die;
+namespace Kntnt\Adminbar_Sanitizer;
 
-require_once __DIR__ . '/classes/class-admin-bar.php';
+defined('ABSPATH') && new Plugin;
 
-new Plugin();
+class Plugin {
 
-final class Plugin {
-
-  public function __construct() {
-    add_action( 'init', [ $this, 'run' ] );
-  }
-
-  public function run() {
-  
-    $remove_list = apply_filters('kntnt_admin_bar_sanitizer_remove_list', [
-      'wp-logo',
-      'about',
-      'wporg',
-      'documentation',
-      'support-forums',
-      'feedback',
-      'site-name',
-      'customize',
-    ]);
-
-    if(is_admin()) {
-
-      $add_list = apply_filters('kntnt_admin_bar_sanitizer_add_admin_list', [
-        'home' => [
-          'title' => get_option('blogname'),
-          'link' => get_home_url() . '/',
-          'icon' => '\f102',
-          'icon_top_margin' => 0,
-        ],
-      ]);
-
-    }
-    else {
-
-      $add_list = apply_filters('kntnt_admin_bar_sanitizer_add_front_list', [
-        'home' => [
-          'title' => get_option('blogname'),
-          'link' => get_home_url() . '/wp-admin/',
-          'icon' => '\f226',
-          'icon_top_margin' => 0,
-        ],
-      ]);
-
+    public function __construct() {
+        add_action('init', [$this, 'run']);
     }
 
-    $ab = new Admin_Bar();
+    public function run() {
 
-    foreach($remove_list as $item) {
-      $ab->remove($item);
-    }
-    
-    foreach($add_list as $item => $args) {
-      $this->default_values($args);
-      $ab->add($item, $args['title'], $args['link'], $args['icon'], $args['icon_top_margin']);
+        $remove_list = apply_filters('kntnt_admin_bar_sanitizer_remove_list', [
+            'wp-logo',
+            'about',
+            'wporg',
+            'documentation',
+            'support-forums',
+            'feedback',
+            'site-name',
+        ]);
+
+        if (is_admin()) {
+
+            $add_list = apply_filters('kntnt_admin_bar_sanitizer_add_admin_list', [
+                'home' => [
+                    'title' => get_option('blogname'),
+                    'link' => get_home_url() . '/',
+                    'icon' => '\f102',
+                    'icon_top_margin' => 0,
+                ],
+            ]);
+
+        } else {
+
+            $add_list = apply_filters('kntnt_admin_bar_sanitizer_add_front_list', [
+                'home' => [
+                    'title' => get_option('blogname'),
+                    'link' => get_home_url() . '/wp-admin/',
+                    'icon' => '\f226',
+                    'icon_top_margin' => 0,
+                ],
+            ]);
+
+        }
+
+        $ab = new Admin_Bar();
+
+        foreach ($remove_list as $item) {
+            $ab->remove($item);
+        }
+
+        foreach ($add_list as $item => $args) {
+
+            if (!isset($args['icon'])) {
+                $args['icon'] = '';
+            }
+            if (!isset($args['icon_top_margin'])) {
+                $args['icon_top_margin'] = 2;
+            }
+
+            $ab->add($item, $args['title'], $args['link'], $args['icon'], $args['icon_top_margin']);
+
+        }
+
     }
 
-  }
-  
-  private function default_values(&$args) {
-    if ( ! isset($args['icon'])) { $args['icon'] = ''; }
-    if ( ! isset($args['icon_top_margin'])) { $args['icon_top_margin'] = 2; }
-  }
+}
+
+abstract class Admin_Bar_Container {
+
+    protected $menu;
+
+    protected $icon;
+
+    private $items = [];
+
+    protected function __construct($parent = '', $id = '', $title = '', $href = '', $icon = '', $icon_top_margin = 2) {
+
+        if ($icon) {
+            $title = $this->icon_html($title);
+            $this->icon = $this->icon_css($id, $icon, $icon_top_margin);
+        };
+
+        $this->menu = [
+            'id' => $id,
+            'title' => $title,
+            'parent' => $parent,
+            'href' => $href,
+        ];
+
+    }
+
+    protected function _add($parent, $id, $title, $href, $icon, $icon_top_margin) {
+        $item = new Admin_Bar_Item($parent, $id, $title, $href, $icon, $icon_top_margin);
+        $this->items[$id] = $item;
+        return $item;
+    }
+
+    protected function _before_admin_bar_render_action($wp_admin_bar) {
+        foreach ($this->items as $item) {
+            $wp_admin_bar->add_node($item->menu);
+            if ($item->icon) {
+                echo $item->icon . PHP_EOL;
+            }
+            $item->_before_admin_bar_render_action($wp_admin_bar);
+        }
+    }
+
+    protected function icon_html($title) {
+        return '<span class="ab-icon"></span>' . $title;
+    }
+
+    protected function icon_css($id, $icon, $icon_top_margin) {
+        return <<<CSS
+      #wp-admin-bar-$id {
+        display: inline-block
+      }
+      #wp-admin-bar-$id .ab-icon::before {
+        content: '$icon';
+        margin-top: {$icon_top_margin}px
+      }
+CSS;
+    }
+
+}
+
+final class Admin_Bar extends Admin_Bar_Container {
+
+    private $trash = [];
+
+    public function __construct() {
+        add_action('add_admin_bar_menus', [$this, 'add_admin_bar_menus_action']);
+        add_action('wp_before_admin_bar_render', [$this, 'before_admin_bar_render_action']);
+    }
+
+    public function add($id, $title, $href, $icon = '', $icon_top_margin = 2) {
+        return $this->_add('', $id, $title, $href, $icon, $icon_top_margin);
+    }
+
+    public function remove($id) {
+        $this->trash[] = $id;
+    }
+
+    public function add_admin_bar_menus_action() {
+        $position = 10;
+        add_action('admin_bar_menu', [$this, 'admin_bar_menu_action'], $position);
+    }
+
+    public function admin_bar_menu_action() {
+
+        // N.B: The global object $wp_admin_bar is only available in
+        // the 'admin_bar_menu' and 'wp_before_admin_bar_render' hooks.
+        global $wp_admin_bar;
+
+        // Add menu items.
+        echo '<style>' . PHP_EOL;
+        $this->_before_admin_bar_render_action($wp_admin_bar);
+        echo '</style>' . PHP_EOL;
+
+    }
+
+    public function before_admin_bar_render_action() {
+
+        // N.B: The global object $wp_admin_bar is only available in
+        // the 'admin_bar_menu' and 'wp_before_admin_bar_render' hooks.
+        global $wp_admin_bar;
+
+        // Remove menu items.
+        foreach ($this->trash as $id) {
+            $wp_admin_bar->remove_node($id);
+        }
+
+    }
+
+}
+
+class Admin_Bar_Item extends Admin_Bar_Container {
+
+    public function add($id, $title, $href, $icon = '', $icon_top_margin = 2) {
+        return $this->_add($this->menu['id'], $id, $title, $href, $icon, $icon_top_margin);
+    }
 
 }
